e710c7837b5da0e2db2eee5cd55586a7e4ca0a9e
nagutabby <62084485+nagutabby@users.noreply.github.com>
2024-06-30 20:01:06 +0900

    円周率を計算するプログラムを追加する

---
+++ calculate_pi.py
@@ -0,0 +1,40 @@
+def f(x: float) -> float:
+    return (4 - x ** 2) ** (1 / 2)
+
+def h(i: int, n: int) -> float:
+    return f(i * (2 / n)) * (2 / n)
+
+def g(i: int, n: int) -> float:
+    return (f(i * (2 / n)) + f((i + 1) * (2 / n))) * (2 / n) * (1 / 2)
+
+def pi_with_rectangle(e: float) -> float:
+    v1: int | float = 0
+    v2: float = f(0) * 2
+    n: int = 1
+    while abs(v1 - v2) > e:
+        n = 2 * n
+        v1 = v2
+        v2 = 0
+        for i in range(n):
+            v2 = v2 + h(i, n)
+    return v2
+
+def pi_with_trapezoido(e: float) -> float:
+    v1: int | float = 0
+    v2: float = (f(0) + f(1)) * 2 * (1 / 2)
+    n: int = 1
+    while abs(v1 - v2) > e:
+        n = 2 * n
+        v1 = v2
+        v2 = 0
+        for i in range(n):
+            v2 = v2 + g(i,n)
+    return v2
+
+print(f'pi is {pi_with_rectangle(0.001)} where e is 0.001.')
+print(f'pi is {pi_with_rectangle(0.000001)} where e is 0.000001.')
+print(f'pi is {pi_with_rectangle(0.00000001)} where e is 0.00000001.')
+
+print(f'pi is {pi_with_trapezoido(0.001)} where e is 0.001.')
+print(f'pi is {pi_with_trapezoido(0.000001)} where e is 0.000001.')
+print(f'pi is {pi_with_trapezoido(0.00000001)} where e is 0.00000001.')
